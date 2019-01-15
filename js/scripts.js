2bf25bcfe7bda6d1dcfbf2e075aef7ff1772fbe6
Drew Yoxsimer and kohichi horii <drew@>
2019-01-15 11:35:04 -0800

    lay basic framework for flashcard grid and answer viewing functionality.

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#javaBtn").click(function() {
+    $("#javaFront").toggle();
+    $("#javaBack").toggle();
+  });
+  $("#oprBtn").click(function() {
+    $("#oprFront").toggle();
+    $("#oprBack").toggle();
+  });
+  $("#varBtn").click(function() {
+    $("#varFront").toggle();
+    $("#varBack").toggle();
+  });
+
+});
