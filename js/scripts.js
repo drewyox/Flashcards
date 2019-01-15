bfaea29162deb541ab35fd3bfaa25a7d4da05536
Drew Yoxsimer and kohichi horii <drew@>
2019-01-15 14:16:02 -0800

    cut way back on code with .this and children.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,82 @@
 $(document).ready(function() {
-  $("#javaBtn").click(function() {
-    $("#javaFront").toggle();
-    $("#javaBack").toggle();
+  // $("#javaBtn").click(function() {
+  //   $("#javaFront").toggle();
+  //   $("#javaBack").toggle();
+  // });
+  // $("#oprBtn").click(function() {
+  //   $("#oprFront").toggle();
+  //   $("#oprBack").toggle();
+  // });
+  // $("#varBtn").click(function() {
+  //   $("#varFront").toggle();
+  //   $("#varBack").toggle();
+  // });
+  // $("#varNBtn").click(function() {
+  //   $("#varNFront").toggle();
+  //   $("#varNBack").toggle();
+  // });
+  // $("#funcBtn").click(function() {
+  //   $("#funcFront").toggle();
+  //   $("#funcBack").toggle();
+  // });
+  // $("#methBtn").click(function() {
+  //   $("#methFront").toggle();
+  //   $("#methBack").toggle();
+  // });
+  // $("#argBtn").click(function() {
+  //   $("#argFront").toggle();
+  //   $("#argBack").toggle();
+  // });
+  // $("#paraBtn").click(function() {
+  //   $("#paraFront").toggle();
+  //   $("#paraBack").toggle();
+  // });
+  // $("#retBtn").click(function() {
+  //   $("#retFront").toggle();
+  //   $("#retBack").toggle();
+  // });
+  // $("#chainBtn").click(function() {
+  //   $("#chainFront").toggle();
+  //   $("#chainBack").toggle();
+  // });
+  // $("#strBtn").click(function() {
+  //   $("#strFront").toggle();
+  //   $("#strBack").toggle();
+  // });
+  // $("#boolBtn").click(function() {
+  //   $("#boolFront").toggle();
+  //   $("#boolBack").toggle();
+  // });
+  // $("#undeBtn").click(function() {
+  //   $("#undeFront").toggle();
+  //   $("#undeBack").toggle();
+  // });
+  // $("#nanBtn").click(function() {
+  //   $("#nanFront").toggle();
+  //   $("#nanBack").toggle();
+  // });
+  // $("#escaBtn").click(function() {
+  //   $("#escaFront").toggle();
+  //   $("#escaBack").toggle();
+  // });
+  // $("#aleBtn").click(function() {
+  //   $("#aleFront").toggle();
+  //   $("#aleBack").toggle();
+  // });
+  // $("#comBtn").click(function() {
+  //   $("#comFront").toggle();
+  //   $("#comBack").toggle();
+  // });
+  // $("#jqueryBtn").click(function() {
+  //   $("#jqueryFront").toggle();
+  //   $("#jqueryBack").toggle();
+  // });
+  // $("#attBtn").click(function() {
+  //   $("#attFront").toggle();
+  //   $("#attBack").toggle();
+  // });
+  $(".switch").click(function() {
+    $(this).children(".front").toggle();
+    $(this).children(".back").toggle();
   });
-  $("#oprBtn").click(function() {
-    $("#oprFront").toggle();
-    $("#oprBack").toggle();
-  });
-  $("#varBtn").click(function() {
-    $("#varFront").toggle();
-    $("#varBack").toggle();
-  });
-
 });
